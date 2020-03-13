96c79678cdbb062f2f3e2e94af8de88b2e2e47aa
joepuzzo <jgpuzzo2@gmail.com>
2020-03-13 09:32:47 -0700

    removed dead code

--- djangoql/polls/schema.py
+++ djangoql/polls/schema.py
@@ -1,18 +1,14 @@
 from graphene_django import DjangoObjectType
 from .models import Question as QuestionModel, Choice as ChoiceModel
-#from rest_framework.permissions import AllowAny
-#from django.views.decorators.csrf import csrf_exempt
 import graphene
 
 class Question(DjangoObjectType):
     class Meta:
         model = QuestionModel
-        #permission_classes = [AllowAny]
 
 class Choice(DjangoObjectType):
     class Meta:
         model = ChoiceModel
-        #permission_classes = [AllowAny]
 
 class Query(graphene.ObjectType):
     questions = graphene.List(Question)
